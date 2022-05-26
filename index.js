f2e675a316d1af9c99be724846e6ea7c9821140f
Endri Dollani <44905521+endridollani@users.noreply.github.com>
2022-05-26 22:54:23 +0200

    Server working and fetching data from mysql

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const routesHandler = require("./routes/handler.js");
+require("dotenv/config");
 
-app.listen(3001,() => {
-    console.log("Server is working")
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/", routesHandler);
+
+const PORT = process.env.PORT || 4000; // backend routing port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
